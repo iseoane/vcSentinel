363abee033b04b41461126dd1891cdc6760a8725
iseoane <iseoane@quental.com>
2026-09-21 19:25:05 +0200

    chore(slice): auto-fragmented test batch #5

--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ISeoane-Quental/vas.sentinel/internal/git"
+	"github.com/ISeoane-Quental/vcSentinel/internal/git"
 )
 
 // TestStoreAtomicWrite reproduces TestLedgerEscrituraAtomica (T2.x) over
